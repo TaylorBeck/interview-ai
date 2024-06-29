2f5e490e030d6bd37e82def81c9d22e12190afd1
Taylor Beck <beck.taylorg@gmail.com>
2024-06-28 21:00:24 -0700

    Add header mobile nav

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -6,6 +6,8 @@ import { ClerkLoading, ClerkLoaded, UserButton } from '@clerk/nextjs'
 import { usePathname } from 'next/navigation'
 import { Skeleton } from '@/components/ui/skeleton'
 
+import { Menu } from 'lucide-react'
+
 function Header() {
   const path = usePathname()
 
@@ -15,7 +17,7 @@ function Header() {
     <div className='flex justify-between items-center bg-secondary shadow-sm p-4'>
       <div className='flex items-center gap-3'>
         <Image src={'/logo.svg'} height={60} width={60} alt='logo' style={{ width: '100%', height: 'auto' }} />
-        <h1 className='font-bold text-2xl text-[#3E488F]'><span className='font-normal'>Interview</span>AI</h1>
+        <h1 className='font-bold text-2xl text-[#3E488F] hidden xs:block'><span className='font-normal'>Interview</span>AI</h1>
       </div>
       <ul className='hidden md:flex gap-6'>
         <li className={`${menuItemStyle} ${path === '/dashboard'&&'text-primary font-semibold'}`}>Dashboard</li>
@@ -23,12 +25,15 @@ function Header() {
         <li className={`${menuItemStyle} ${path === '/dashboard/pricing'&&'text-primary font-semibold'}`}>Pricing</li>
         <li className={`${menuItemStyle} ${path === 'dashboard/how'&&'text-primary font-semibold'}`}>How It Works</li>
       </ul>
-      <ClerkLoading>
-        <Skeleton className="h-[28px] w-[28px] bg-[#5F6BBA] rounded-full" />
-      </ClerkLoading>
-      <ClerkLoaded>
-        <UserButton />
-      </ClerkLoaded>
+      <div className='flex items-center gap-4'>
+        <Menu className='md:hidden h-6 w-6 text-slate-500 hover:scale-105 hover:cursor-pointer' />
+        <ClerkLoading>
+          <Skeleton className="h-[28px] w-[28px] bg-[#5F6BBA] rounded-full" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
+      </div>
     </div>
   )
 }
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
   return (
     <div className="p-10">
       <h1 className="font-bold text-3xl mb-2">Dashboard</h1>
-      <h1 className="text-slate-500">Create and Manage Your AI Mock Interviews</h1>
+      <h1 className="text-slate-500">Create and Manage AI Mock Interviews</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddInterview />
