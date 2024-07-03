3b744beed4e2fa18ca843d7ff6cebe67653a517c
Taylor Beck <beck.taylorg@gmail.com>
2024-07-03 00:52:27 -0700

    Add webcam to interview details page

--- app/dashboard/interviews/[id]/page.jsx
+++ app/dashboard/interviews/[id]/page.jsx
@@ -3,11 +3,36 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 
+import { toast } from "sonner";
+import Webcam from 'react-webcam';
+import { WebcamIcon, ShieldAlert, X, Play, Pause } from 'lucide-react';
+
+import {
+  Skeleton
+} from '@/components/ui/skeleton';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '@/components/ui/alert'
+
 export default function Interview() {
   const [interview, setInterview] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const params = useParams();
+
+  const [showWebcam, setShowWebcam] = useState(false);
+  const [showAlert, setShowAlert] = useState(true);
 
   useEffect(() => {
     const fetchInterview = async () => {
@@ -15,7 +40,7 @@ export default function Interview() {
         const response = await fetch(`/api/interviews/${params.id}`);
         if (!response.ok) {
           throw new Error('Failed to fetch interview');
-        }
+      }
         const data = await response.json();
         setInterview(data.interview);
       } catch (error) {
@@ -28,28 +53,100 @@ export default function Interview() {
     fetchInterview();
   }, [params.id]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-  if (!interview) return <div>Interview not found</div>;
+  const onWebcamError = (error) => {
+    setShowWebcam(false);
+    toast("Webcam Error", {
+      description: error.message
+    });
+  }
 
   return (
-    <div>
-      <h1>Interview Details</h1>
-      <p>
-        {interview.interviewId}
-      </p>
-      <p>
-        {interview.jobTitle}
-      </p>
-      <p>
-        {interview.jobDescription}
-      </p>
-      <p>
-        {interview.jobExperience}
-      </p>
-      <p>
-        {interview.createdAt}
-      </p>
+    <>
+      <div className="flex justify-between mt-12">
+        <div className="flex flex-col justify-between items-start">
+          {interview ?
+            <Card className="w-[400px]">
+              <CardHeader>
+                <CardTitle className="mb-3">{interview.jobTitle}</CardTitle>
+                <CardDescription>{interview.jobExperience} - {interview.jobDescription}</CardDescription>
+              </CardHeader>
+              <CardContent>
+
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button
+                  variant={showWebcam ? 'destructive' : ''}
+                  onClick={() => setShowWebcam(!showWebcam)}
+                >
+                  {showWebcam ?
+                    <>
+                      <Pause fill='white' className="w-4 h-4 mr-1" />
+                      Pause
+                    </>
+                  : 
+                    <>
+                      <Play fill='white' className="w-4 h-4 mr-1" />
+                      Start
+                    </>
+                  }
+                </Button>
+              </CardFooter>
+            </Card>
+          :
+            <Skeleton className="w-[400px] h-[320px] rounded-lg" />
+          }
+        </div>
+          {showWebcam ?
+            <Webcam
+              onUserMedia={() => setShowWebcam(true)}
+              onUserMediaError={(error) => onWebcamError(error)}
+              mirrored={true}
+              videoConstraints={
+                {
+                  width: 400,
+                  height: 320,
+                  facingMode: "user"
+                }
+              }
+              style={{
+                borderRadius: 8
+              }}
+            /> 
+          :
+            <div className="flex justify-center items-center w-[400px] h-[320px] border-2 border-dashed border-gray-300 rounded-md">
+              <WebcamIcon
+                className="h-24 w-24 p-6 text-gray-300"
+                onClick={() => setShowWebcam(true)}
+              />
+            </div>
+          }
       </div>
+      <Alert
+        className="bg-yellow-100 mt-8"
+        style={{
+          zIndex: 1,
+          top: "0",
+          right: "0",
+          bottom: "0",
+          left: "0",
+          opacity: !showAlert ? "0" : "1",
+          transition: "all .2s",
+          visibility: !showAlert ? "hidden" : "visible",
+        }}
+      >
+        <ShieldAlert className="h-5 w-5" />
+        <AlertTitle>
+          Heads up!
+        </AlertTitle>
+        <AlertDescription>
+          <X
+            className="absolute top-2 right-2 w-4 h-4 hover:cursor-pointer hover:scale-105 hover:-rotate-3"
+            onClick={() => setShowAlert(false)}
+          />
+          We never record your video. Instead, we transcribe the audio and save your answers in a text format. Your answers can be deleted or exported at any time.
+        </AlertDescription>
+      </Alert>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -75,6 +75,12 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      scale: {
+        "101": "101%",
+        "102": "102%",
+        "103": "103%",
+        "104": "104%"
       }
     }
   },
